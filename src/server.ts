d0e18b0bb5d3afe6e8eda99d7198f44ac6141065
Goutham <gouthamrs4u@gmail.com>
2024-04-08 19:11:26 +0530

    Consoles for debugging

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 
 import serverConfig from "./utils/config-validate";
 import app from "./app";
+const { rss, heapUsed } = process.memoryUsage();
 
 // import config from "config";
 // const dbConf = config.get("db");
@@ -15,40 +16,53 @@ const PORT = serverConfig.port;
 const bootstrap = async () => {
     /* Server startup */
     const serverApp = await app();
-    const server = serverApp.listen(PORT, async () => {
-    // tslint:disable-next-line: no-console
-        console.log("\n");
-        // tslint:disable-next-line: no-console
-        console.log(
-            TAG + "App  is running in %s mode at http://localhost:%d",
-            serverConfig.serverEnvironment,
-            PORT,
-        );
-
-        // return connectDatabase().catch((error) => {
-        //     console.error("Couldn't connect to the database!");
-        //     console.error(error);
-        // });
-    });
-
-    const gracefulShutdown = async (callType: any) => {
-        console.error(TAG + `${callType} signal received.`);
-
-        // Stops the server from accepting new connections and finishes existing connections.
-        server.close(function(err: any) {
-            if (err) {
-                console.error(TAG, "server error", err);
-                process.exit(1);
-            } else {
-                process.exit(0);
-            }
+    try {
+        console.log("11111");
+        console.log(`Resident Set Size (RSS): ${rss / 1024 / 1024} MB`);
+        console.log(`Heap Used: ${heapUsed / 1024 / 1024} MB`);
+        const server = serverApp.listen(PORT, async () => {
+            // tslint:disable-next-line: no-console
+            console.log("\n");
+            // tslint:disable-next-line: no-console
+            console.log(
+                TAG + "App  is running in %s mode at http://localhost:%d",
+                serverConfig.serverEnvironment,
+                PORT,
+            );
+        
+            // return connectDatabase().catch((error) => {
+            //     console.error("Couldn't connect to the database!");
+            //     console.error(error);
+            // });
         });
-    };
 
-    process.on("SIGINT", async () => gracefulShutdown("SIGINT"));
+        const gracefulShutdown = async (callType: any) => {
+            console.error(TAG + `${callType} signal received.`);
+    
+            // Stops the server from accepting new connections and finishes existing connections.
+            server.close(function(err: any) {
+                if (err) {
+                    console.error(TAG, "server error", err);
+                    process.exit(1);
+                } else {
+                    process.exit(0);
+                }
+            });
+        };
+    
+        process.on("SIGINT", async () => gracefulShutdown("SIGINT"));
+    
+        process.on("SIGINT", async () => gracefulShutdown("SIGINT"));
+        return server;
+    }
+    catch(e){
+        console.log(`Resident Set Size (RSS): ${rss / 1024 / 1024} MB`);
+        console.log(`Heap Used: ${heapUsed / 1024 / 1024} MB`);
 
-    process.on("SIGINT", async () => gracefulShutdown("SIGINT"));
-    return server;
+        console.log("err in server", e);
+    }
+
+
 };
 
 export default bootstrap();
